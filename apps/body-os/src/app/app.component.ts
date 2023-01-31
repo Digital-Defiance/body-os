5d722432821840987928352cd416b8b5b8f94703
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2023-01-31 15:40:47 -0800

    initial repo

---
+++ apps/body-os/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'body-os-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'body-os';
+}
